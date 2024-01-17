3d2866aab5a97db8e2889566f487e3264d795a7e
BiaAkemi <beancaakemi@gmail.com>
2024-01-17 17:46:37 -0300

    Add bibliotecas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import Home from './assets/pages/Home/Home'
+import React from 'react';
+import Home from './pages/Home/Home';
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Home/Login';
 
-const App = () => {
+function App() {
   return (
     <>
-    <Home />
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-    )
+);
 }
-
-export default App
+export default App;
