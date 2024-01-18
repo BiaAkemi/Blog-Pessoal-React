756b33d69a7018951bc29ef68f24b5550de9f6f0
BiaAkemi <beancaakemi@gmail.com>
2024-01-18 10:23:20 -0300

    Adding context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import Home from './pages/Home/Home';
-import Navbar from './components/navBar/NavBar';
-import Footer from './components/footer/Footer';
+import UserProvider from './contexts/UserContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/navBar/NavBar';
 import Login from './pages/Home/Login';
+import Home from './pages/Home/Home';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
     <>
+    <UserProvider>
     <BrowserRouter>
         <Navbar />
           <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
+          <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+
           </div>
           <Footer />
         </BrowserRouter>
+        </UserProvider>
     </>
 );
 }
-export default App;
+export default App; 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React,  { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+import { Link, useNavigate } from 'react-router-dom';
 import homeLogo from '../../assets/cactus.svg'
 import './Home.css';
 
 function Home() {
+  const { nome, setNome } = useContext(UserContext);
     return (
         <>
         <div className="bg-green-400 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+              <h2 className='text-5xl font-bold'>Seja bem vinde, {nome}!</h2>
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
-  
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+                <button className='rounded bg-[#e7ee83] text-[rgb(74,177,86)] py-2 px-4'>Ver postagens</button>
+                <Link to="/login" className='rounded bg-[#e7ee83] text-[rgb(74,177,86)] py-2 px-4'>
+              Voltar 
+              </Link>
               </div>
             </div>
   
