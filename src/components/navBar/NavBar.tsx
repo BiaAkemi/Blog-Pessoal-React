100b8933ad44f900131a7c3b5d1830bb8c663997
BiaAkemi <beancaakemi@gmail.com>
2024-01-29 12:37:29 -0300

    CRUD Postagens

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -20,16 +20,16 @@ function Navbar() {
     <>
      <div className='w-full bg-[#483D8B] text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
             <div className='flex gap-4'>
               <Link to='/login' className='hover:underline'>Login</Link>
               <Link to='/home' className='hover:underline'>Home</Link>
-              <div className='hover:underline'>Postagens</div>
-              <div className='hover:underline'>Temas</div>
+              <Link to='/postagens' className='hover:underline'>Postagens</Link>
+              <Link to='/temas' className='hover:underline'>Temas</Link>
               <Link to='cadastroTema' className='hover:underline'>Cadastrar tema</Link>
               <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
             </div>
           </div>
         </div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,23 @@
-import React,  { useContext } from 'react';
-import { UserContext } from '../../contexts/UserContext';
+import React from 'react';
 import homeLogo from '../../assets/cactus.svg'
 import './Home.css';
-import { Link, useNavigate } from 'react-router-dom';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+
 
 
 function Home() {
-  const { nome, setNome } = useContext(UserContext);
-    return (
-        <>
+  return (
+      <>
         <div className="bg-[#9370DB] flex justify-center">
-          <div className='container grid grid-cols-2 text-white'>
-            <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde, {nome}!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+        <div className='container grid grid-cols-2 text-white'>
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+            <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+
               <div className="flex justify-around gap-4">
-                <button className='rounded bg-[#6A5ACD] hover:bg-[#483D8B]  text-[#ffffff] py-2 px-4'>Ver postagens</button>
+              <ModalPostagem />
+              <button className='rounded bg-[#6A5ACD] hover:bg-[#483D8B]  text-[#ffffff] py-2 px-4'>Ver postagens</button>
               </div>
             </div>
   
@@ -25,7 +27,7 @@ function Home() {
             </div>
           </div>
         </div>
-      
+        <ListaPostagens />
       </>
     );
 }
