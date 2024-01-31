1e18657a8203e84bb4a0f10f89fa95e37f1e2e7d
BiaAkemi <beancaakemi@gmail.com>
2024-01-31 11:19:22 -0300

    Deploy do projeto Blog Pessoal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,7 +15,7 @@ function Home() {
 
               <div className="flex justify-around gap-4">
                 <ModalPostagem />   
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>
+                <button className='rounded bg-white text-[#483D8B] py-2 px-4'>
                   <Link to="/postagens">Ver Postagens</Link> 
                 </button>
               </div>
